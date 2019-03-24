bb0309808d09dce0798b5b4d46dffcec3b371b65
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2019-03-24 22:04:19 +0100

    Add changes

--- js/index.js
+++ js/index.js
@@ -24,4 +24,26 @@ $(function () {
     var world_data = Highcharts.geojson(Highcharts.maps['custom/world']);
     var us_data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']);
     var india_data = Highcharts.geojson(Highcharts.maps['countries/in/custom/in-all-disputed']);
+
+    $.each(world_data, function(i) {
+        if(this.properties[property] == 'us') {
+            this.drilldown = getDrilldown(
+                us_data
+            );
+        }
+    })
+    this.valude = visited_countries.indexOf(this.properties[property]);
 });
+
+$('#containers').highcharts('Map',{
+    chart: {
+        spacingBottom: 20, 
+        events: {
+            drilldown: function (e) {
+                if(!e.seriesOptions){
+                
+                }
+            }
+        }
+    }
+})
